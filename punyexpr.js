a1fb72e0dfa0fb5e987712ecc8e42e9f1b30a638
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-11-20 09:27:09 -0500

    Implement boolean

--- punyexpr.js
+++ punyexpr.js
@@ -1,12 +1,15 @@
 (function (exports) {
   'use strict'
 
-  const TOKEN_REGEXP_SINGLEQUOTE_STRING = '\'((?:[^\'\\\\]|\\\\.)*)\''
-  const TOKEN_REGEXP_DOUBLEQUOTE_STRING = '"((?:[^"\\\\]|\\\\.)*)"'
+  const TOKEN_REGEXP_SINGLE_QUOTE_STRING = '\'((?:[^\'\\\\]|\\\\.)*)\''
+  const TOKEN_REGEXP_DOUBLE_QUOTE_STRING = '"((?:[^"\\\\]|\\\\.)*)"'
   const TOKEN_TYPE_STRING = 'string'
 
   const TOKEN_REGEXP_NUMBER = '((?:-|\\+)?(?:\\d+(?:\\.\\d*)?|\\.\\d+))'
   const TOKEN_TYPE_NUMBER = 'number'
+
+  const TOKEN_REGEXP_BOOLEAN = '(true|false)'
+  const TOKEN_TYPE_BOOLEAN = 'boolean'
 
   const TOKEN_REGEXP_UNKNOWN = '(.+)'
   const TOKEN_TYPE_UNKNOWN = 'unknown'
@@ -15,37 +18,43 @@
     TOKEN_TYPE_STRING,
     TOKEN_TYPE_STRING,
     TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_BOOLEAN,
 
     TOKEN_TYPE_UNKNOWN
   ]
 
   const TOKENIZER_REGEXP = new RegExp([
-    TOKEN_REGEXP_SINGLEQUOTE_STRING,
-    TOKEN_REGEXP_DOUBLEQUOTE_STRING,
+    TOKEN_REGEXP_SINGLE_QUOTE_STRING,
+    TOKEN_REGEXP_DOUBLE_QUOTE_STRING,
     TOKEN_REGEXP_NUMBER,
+    TOKEN_REGEXP_BOOLEAN,
 
     TOKEN_REGEXP_UNKNOWN
   ].join('|'), 'g')
+
+  const TOKENIZER_CONVERTER = [
+    value => value.replace(/\\'/g, '\''), // TOKEN_REGEXP_SINGLE_QUOTE_STRING
+    value => value.replace(/\\"/g, '"'), // TOKEN_REGEXP_DOUBLE_QUOTE_STRING
+    value => parseFloat(value), // TOKEN_REGEXP_NUMBER
+    value => value === 'true' // TOKEN_REGEXP_BOOLEAN
+  ]
 
   const tokenize = (string) => {
     const tokens = []
     string.replace(TOKENIZER_REGEXP, (match, ...capturedValues) => {
       const index = capturedValues.findIndex(capturedValue => capturedValue !== undefined)
       let value = capturedValues[index]
-      if (index === 0) {
-        value = value.replace(/\\'/g, '\'')
-      } else if (index === 1) {
-        value = value.replace(/\\"/g, '"')
-      } else if (index === 2) {
-        value = parseFloat(value)
+      const converter = TOKENIZER_CONVERTER[index]
+      if (converter) {
+        value = converter(value)
       }
       tokens.push([TOKEN_TYPES[index], value])
     })
     return tokens
   }
 
-  const punyexpr = (expr) => {
-  }
+  const punyexpr = {} /* (expr) => {
+  } */
 
   punyexpr.tokenize = tokenize
 
