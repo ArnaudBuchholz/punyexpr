be58f25773b0a01c25a49c5da05865155e5f8942
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-11-23 08:32:18 -0500

    Testing errors

--- tests/errors.spec.js
+++ tests/errors.spec.js
@@ -12,4 +12,23 @@ describe('errors', () => {
       expect(error.message).toBe('Invalid token @12')
     })
   })
+
+  describe('UnexpectedTokenError', () => {
+    it('exposes offset', () => {
+      const error = new punyexpr.UnexpectedTokenError(12)
+      expect(error.offset).toBe(12)
+    })
+
+    it('exposes message', () => {
+      const error = new punyexpr.UnexpectedTokenError(12)
+      expect(error.message).toBe('Unexpected token @12')
+    })
+  })
+
+  describe('EndOfExpressionError', () => {
+    it('exposes message', () => {
+      const error = new punyexpr.UnexpectedTokenError(12)
+      expect(error.message).toBe('Unexpected end of expression')
+    })
+  })
 })
