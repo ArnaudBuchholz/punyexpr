2d6291543d6e4ca18b92835444e7c89007a887ff
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-11-25 23:26:26 -0500

    Refactored tokenizer Adds JS keywords

--- punyexpr.js
+++ punyexpr.js
@@ -1,58 +1,9 @@
 (function (exports) {
   'use strict'
 
-  const TOKEN_REGEXP_SINGLE_QUOTE_STRING = /'((?:[^'\\]|\\.)*)'/
-  const TOKEN_REGEXP_DOUBLE_QUOTE_STRING = /"((?:[^"\\]|\\.)*)"/
-  const TOKEN_TYPE_STRING = 'string'
-
-  const TOKEN_REGEXP_NUMBER = /((?:\d+(?:\.\d*)?|\.\d+))/
-  const TOKEN_TYPE_NUMBER = 'number'
-
-  const TOKEN_REGEXP_BOOLEAN = /(true|false)/
-  const TOKEN_TYPE_BOOLEAN = 'boolean'
-
-  const TOKEN_REGEXP_IDENTIFIER = /([a-zA-Z_][a-zA-Z_0-9]*)/
+  const TOKEN_TYPE_LITERAL = 'literal'
   const TOKEN_TYPE_IDENTIFIER = 'identifier'
-
-  const TOKEN_REGEXP_SYMBOL = /(\+|-|\*|\/|\[|\]|\.|\?|:)/
   const TOKEN_TYPE_SYMBOL = 'symbol'
-
-  const TOKEN_REGEXP_SEPARATOR = /(\s)/
-  const TOKEN_TYPE_SEPARATOR = {}
-
-  const TOKEN_REGEXP_UNKNOWN = /(.)/
-  const TOKEN_TYPE_UNKNOWN = {}
-
-  const TOKEN_TYPES = [
-    TOKEN_TYPE_STRING,
-    TOKEN_TYPE_STRING,
-    TOKEN_TYPE_NUMBER,
-    TOKEN_TYPE_BOOLEAN,
-    TOKEN_TYPE_IDENTIFIER,
-    TOKEN_TYPE_SYMBOL,
-
-    TOKEN_TYPE_SEPARATOR,
-    TOKEN_TYPE_UNKNOWN
-  ]
-
-  const TOKENIZER_REGEXP = new RegExp([
-    TOKEN_REGEXP_SINGLE_QUOTE_STRING,
-    TOKEN_REGEXP_DOUBLE_QUOTE_STRING,
-    TOKEN_REGEXP_NUMBER,
-    TOKEN_REGEXP_BOOLEAN,
-    TOKEN_REGEXP_IDENTIFIER,
-    TOKEN_REGEXP_SYMBOL,
-
-    TOKEN_REGEXP_SEPARATOR,
-    TOKEN_REGEXP_UNKNOWN
-  ].map(re => { const str = re.toString(); return str.substring(1, str.length - 1) }).join('|'), 'g')
-
-  const TOKENIZER_CONVERTER = [
-    value => value.replace(/\\'/g, '\''), // TOKEN_REGEXP_SINGLE_QUOTE_STRING
-    value => value.replace(/\\"/g, '"'), // TOKEN_REGEXP_DOUBLE_QUOTE_STRING
-    value => parseFloat(value), // TOKEN_REGEXP_NUMBER
-    value => value === 'true' // TOKEN_REGEXP_BOOLEAN
-  ]
 
   class PunyExprError extends Error {
     constructor (name, message, offset) {
@@ -65,84 +16,115 @@
     throw new PunyExprError(name, message, offset)
   }
 
-  // const TOKEN_TYPE = 0
-  // const TOKEN_VALUE = 1
-  const TOKEN_OFFSET = 2
-
   const tokenize = (string) => {
+    // Removed false,null,undefined,true,typeof
+    const JS_FORBIDDEN_KEYWORDS = 'abstract,arguments,await,boolean,break,byte,case,catch,char,class,const,continue,debugger,default,delete,do,double,else,enum,eval,export,extends,final,finally,float,for,function,goto,if,implements,import,in,instanceof,int,interface,let,long,native,new,package,private,protected,public,return,short,static,super,switch,synchronized,this,throw,throws,transient,try,var,void,volatile,while,with,yield'.split(',')
+
+    const TOKEN_REGEXP_SINGLE_QUOTE_STRING = /'((?:[^'\\]|\\.)*)'/
+    const TOKEN_REGEXP_DOUBLE_QUOTE_STRING = /"((?:[^"\\]|\\.)*)"/
+    const TOKEN_REGEXP_NUMBER = /((?:\d+(?:\.\d*)?|\.\d+))/
+
+    const TOKEN_REGEXP_IDENTIFIER = /([a-zA-Z_][a-zA-Z_0-9]*)/
+
+    const TOKEN_REGEXP_SYMBOL = /(\+|-|\*|\/|\[|\]|\.|\?|:)/
+
+    const TOKEN_REGEXP_SEPARATOR = /(\s)/
+
+    const TOKEN_REGEXP_UNKNOWN = /(.)/
+
+    const TOKENIZER_REGEXP = new RegExp([
+      TOKEN_REGEXP_SINGLE_QUOTE_STRING,
+      TOKEN_REGEXP_DOUBLE_QUOTE_STRING,
+      TOKEN_REGEXP_NUMBER,
+      TOKEN_REGEXP_IDENTIFIER,
+      TOKEN_REGEXP_SYMBOL,
+
+      TOKEN_REGEXP_SEPARATOR,
+      TOKEN_REGEXP_UNKNOWN
+    ].map(re => { const str = re.toString(); return str.substring(1, str.length - 1) }).join('|'), 'g')
+
     const invalidTokenError = offset => PunyExprError.throw('InvalidTokenError', `Invalid token @${offset}`, offset)
+
+    const IDENTIFIER_TO_LITERAL = {
+      true: true,
+      false: false,
+      undefined,
+      null: null
+    }
+
+    const TOKENIZER_CONVERTER = [
+      value => [TOKEN_TYPE_LITERAL, value.replace(/\\'/g, '\'')],
+      value => [TOKEN_TYPE_LITERAL, value.replace(/\\"/g, '"')],
+      value => [TOKEN_TYPE_LITERAL, parseFloat(value)],
+      (value, offset) => {
+        if (Object.prototype.hasOwnProperty.call(IDENTIFIER_TO_LITERAL, value)) {
+          return [TOKEN_TYPE_LITERAL, IDENTIFIER_TO_LITERAL[value]]
+        }
+        if (JS_FORBIDDEN_KEYWORDS.includes(value)) {
+          invalidTokenError(offset)
+        }
+        return [TOKEN_TYPE_IDENTIFIER, value]
+      },
+      value => [TOKEN_TYPE_SYMBOL, value],
+      value => [],
+      (value, offset) => invalidTokenError(offset)
+    ]
 
     const tokens = []
     let offset = 0
-    let lastTokenType = TOKEN_TYPE_SEPARATOR
-    const separatorLess = [TOKEN_TYPE_SEPARATOR, TOKEN_TYPE_SYMBOL]
+    let lastTokenType
+    const requireSeparator = [TOKEN_TYPE_LITERAL, TOKEN_TYPE_IDENTIFIER]
     string.replace(TOKENIZER_REGEXP, (match, ...capturedValues) => {
       const rawType = capturedValues.findIndex(capturedValue => capturedValue !== undefined)
-      const rawValue = capturedValues[rawType]
-      let value
+      const capturedValue = capturedValues[rawType]
       const converter = TOKENIZER_CONVERTER[rawType]
-      if (converter) {
-        value = converter(rawValue)
-      } else {
-        value = rawValue
-      }
-      const type = TOKEN_TYPES[rawType]
-      if (!separatorLess.includes(type) && !separatorLess.includes(lastTokenType)) {
+      const [type, value] = converter(capturedValue, offset)
+      if (requireSeparator.includes(type) && requireSeparator.includes(lastTokenType)) {
         invalidTokenError(offset)
       }
-      lastTokenType = TOKEN_TYPES[rawType]
-      if (lastTokenType === TOKEN_TYPE_UNKNOWN) {
-        invalidTokenError(offset)
+      lastTokenType = type
+      if (type !== undefined) {
+        tokens.push([type, value, offset])
       }
-      tokens.push([lastTokenType, value, offset])
-      offset += rawValue.length
+      offset += capturedValue.length
     })
-    return tokens.filter(([type]) => type !== TOKEN_TYPE_SEPARATOR)
-  }
-
-  const bind = (impl, ...args) => Object.assign(impl.bind(null, ...args), { op: impl.name, args })
-
-  const impl = {
-    constant (value) {
-      return value
-    },
-
-    _reduce (op, args) {
-      const context = args.pop()
-      const first = args.shift()
-      return args.reduce((sum, arg) => op(sum, arg, context), first(context))
-    },
-
-    add (...args) {
-      return impl._reduce((total, arg, context) => total + arg(context), args)
-    },
-
-    sub (...args) {
-      return impl._reduce((total, arg, context) => total - arg(context), args)
-    },
-
-    get (member, context) {
-      return context[member(context)]
-    },
-
-    ternary (condition, trueValue, falseValue, context) {
-      if (condition(context)) {
-        return trueValue(context)
-      }
-      return falseValue(context)
-    }
+    return tokens
   }
 
   const parse = (tokens) => {
+    const bind = (impl, ...args) => Object.assign(impl.bind(null, ...args), { op: impl.name, args })
+
+    const impl = {
+      constant (value) {
+        return value
+      },
+
+      add (value1, value2, context) {
+        return value1(context) + value2(context)
+      },
+
+      sub (value1, value2, context) {
+        return value1(context) - value2(context)
+      },
+
+      get (member, context) {
+        return context[member(context)]
+      },
+
+      ternary (condition, trueValue, falseValue, context) {
+        if (condition(context)) {
+          return trueValue(context)
+        }
+        return falseValue(context)
+      }
+    }
+
     const current = () => tokens[0]
-    const offset = () => current()[TOKEN_OFFSET]
+    const offset = () => current()[2]
     const shift = (steps = 1) => {
-      // if (tokens.length < steps) {
-      //   throw new endOfExpressionError()
-      // }
-      const result = tokens.slice(0, steps)
+      const beforeSlicing = tokens
       tokens = tokens.slice(steps)
-      return result
+      return beforeSlicing
     }
     const isSymbol = (expected = undefined) => {
       const [type, value] = current() || []
