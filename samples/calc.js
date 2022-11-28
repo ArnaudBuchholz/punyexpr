616c092728b057e925f0691bac838d96e06fdb3f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-11-27 21:51:29 -0500

    Sample

---
+++ samples/calc.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', () => {
+  const input = document.querySelector('input')
+  const output = document.querySelector('pre')
+  const [exprButton, evalButton] = document.querySelectorAll('button')
+  input.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+      exprButton.click()
+    }
+  })
+  const onClick = (button, method) => button.addEventListener('click', () => {
+    output.innerHTML = ''
+    try {
+      const expr = method(input.value)
+      output.appendChild(document.createTextNode(expr()))
+    } catch (e) {
+      output.appendChild(document.createTextNode(e.toString()))
+    }
+  })
+  onClick(exprButton, punyexpr)
+  onClick(evalButton, eval)
+})
