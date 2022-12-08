6988403684629f820eaba34ce42bf04d37933e15
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-12-08 08:25:30 -0500

    Update sample to show range of parsed expression

--- samples/calc.js
+++ samples/calc.js
@@ -6,19 +6,21 @@ window.addEventListener('load', () => {
   const treeRoot = document.querySelector('.tf-tree ul')
 
   function toAst (parent, object) {
-    const addChild = title => {
+    const addChild = (title, at, length) => {
       const li = parent.appendChild(document.createElement('li'))
       const span = li.appendChild(document.createElement('span'))
       span.className = 'tf-nc'
       span.appendChild(document.createTextNode(title))
+      span.dataset.at = at
+      span.dataset.length = length
       return li
     }
-    const op = Object.keys(object)[0]
-    const args = object[op]
+    const { op, args, at, length } = object
+    let li
     if (op === 'constant') {
-      addChild(JSON.stringify(args[0])).className = 'constant'
+      addChild(JSON.stringify(args[0]), at, length).className = 'constant'
     } else {
-      const li = addChild(op)
+      li = addChild(op, at, length)
       li.className = 'op'
       const ul = li.appendChild(document.createElement('ul'))
       args.forEach(arg => {
@@ -30,6 +32,17 @@ window.addEventListener('load', () => {
       })
     }
   }
+
+  window.addEventListener('mouseover', event => {
+    const { target } = event
+    if (target.className === 'tf-nc') {
+      const { at: rawAt, length: rawLength } = target.dataset
+      const at = Number(rawAt)
+      const length = Number(rawLength)
+      input.focus()
+      input.setSelectionRange(at, at + length)
+    }
+  })
 
   function run () {
     location.hash = encodeURIComponent(input.value)
