f369c9964f370cc7d51c4fa050a67046b647cd0f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-12-10 23:45:43 -0500

    New design

--- samples/calc.js
+++ samples/calc.js
@@ -1,7 +1,7 @@
 /* global location */
 
 window.addEventListener('load', () => {
-  const input = document.querySelector('input')
+  const input = document.querySelector('.input')
   const output = document.querySelector('.output')
   const treeRoot = document.querySelector('.tf-tree ul')
 
@@ -9,18 +9,20 @@ window.addEventListener('load', () => {
     const addChild = (title, at, length) => {
       const li = parent.appendChild(document.createElement('li'))
       const span = li.appendChild(document.createElement('span'))
-      span.className = 'tf-nc'
+      span.className = 'tf-nc '
       span.appendChild(document.createTextNode(title))
       span.dataset.at = at
       span.dataset.length = length
-      return li
+      return { li, span }
     }
-    const { op, args, at, length } = object
-    let li
+    const { op, args, at, length } = object || true
     if (op === 'constant') {
-      addChild(JSON.stringify(args[0]), at, length).className = 'constant'
+      const value = JSON.stringify(args[0])
+      const { li, span } = addChild(value, at, length)
+      li.className = 'constant'
+      span.className += typeof args[0]
     } else {
-      li = addChild(op, at, length)
+      const { li } = addChild(op, at, length)
       li.className = 'op'
       const ul = li.appendChild(document.createElement('ul'))
       args.forEach(arg => {
@@ -35,36 +37,51 @@ window.addEventListener('load', () => {
 
   window.addEventListener('mouseover', event => {
     const { target } = event
-    if (target.className === 'tf-nc') {
+    if (target.className.includes('tf-nc')) {
       const { at: rawAt, length: rawLength } = target.dataset
       const at = Number(rawAt)
       const length = Number(rawLength)
       input.focus()
-      input.setSelectionRange(at, at + length)
+      const sel = window.getSelection()
+      const range = document.createRange()
+      range.setStart(input.firstChild, at)
+      range.setEnd(input.firstChild, at + length)
+      sel.removeAllRanges()
+      sel.addRange(range)
     }
   })
 
   function run () {
-    location.hash = encodeURIComponent(input.value)
+    location.hash = encodeURIComponent(input.innerText)
     output.innerHTML = ''
     treeRoot.innerHTML = ''
     try {
-      const expr = punyexpr(input.value)
-      output.appendChild(document.createTextNode(expr()))
+      const expr = punyexpr(input.innerText)
+      // https://stackoverflow.com/questions/6249095/how-to-set-the-caret-cursor-position-in-a-contenteditable-element-div
+      const sel = window.getSelection()
+      console.log(sel)
+      // input.innerHTML = `<span>${input.innerText}</span>`
+      const result = expr()
+      const typeOfResult = typeof result
+      if (typeOfResult === 'function') {
+        output.appendChild(document.createTextNode(result.toString()))
+      } else {
+        output.appendChild(document.createTextNode(JSON.stringify(result)))
+      }
+      output.className = `output ${typeOfResult}`
       toAst(treeRoot, expr.toJSON())
     } catch (e) {
       output.appendChild(document.createTextNode(e.toString()))
+      output.className = 'output error'
     }
   }
-  document.querySelector('button').addEventListener('click', run)
+
   input.addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
-      run()
-    }
+    setTimeout(run, 0)
   })
 
   if (location.hash) {
-    input.value = decodeURIComponent(location.hash.substring(1))
+    input.innerText = decodeURIComponent(location.hash.substring(1))
     run()
   }
 })
