f3c26e7c2506a3ed67b8e0e21c6879eae2404923
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-05-15 08:05:27 -0400

    doc: types definition

--- dist/punyexpr.d.ts
+++ dist/punyexpr.d.ts
@@ -1,25 +1,41 @@
-declare global {
-  interface globalThis {
-    punyexpr: {
-      (expression: string, options?: {
-        regex: true | ((pattern: string, flags: string) => RegExp);
-      }): (context: { [name in string]: any }) => any;
-      Error: typeof PunyExprError;
-      version: string;
-      tokenize(expression: string): PunyExprToken[];
-    }
+declare module 'punyexpr' {
+
+  type PunyExprToken = 
+    | ['literal', string | number | boolean, offset: number, length: number]
+    | ['identifier', string, offset: number, length: number]
+    | ['regex', [pattern: string, flags: string], offset: number, length: number]
+    | ['symbol', '+' | '-' | '*' | '/' | '[' | ']' | '.' | '?' | ':' | '%' | '<' | '=' | '>' | '!' | '&' | '|' | '(' | ')' | ',', offset: number, length: 1]
+
+  class PunyExprError extends Error {
+    offset: number
   }
+
+  type PunyExprNode = { at: number; length: number } & (
+    | { op: 'constant', args: [value: string | number | boolean] }
+    | { op: 'array', args: PunyExprNode[] }
+    // TODO missing regex
+    | { op: 'context', args: [name: string] }
+    | { op: 'property', args: [context: PunyExprNode, name: string] }
+    | { op: 'call', args: [thisValue: PunyExprNode, args: PunyExprNode[]] }
+    | { op: 'pos' | 'neg' | 'not' | 'getTypeof', args: [PunyExprNode] }
+    | { op: 'exp' | 'mul' | 'div' | 'remainder' | 'add' | 'sub' | 'lt' | 'lte' | 'gt' | 'gte' | 'eq' | 'neq' | 'eqq' | 'neqq' | 'and' | 'or', args: [PunyExprNode, PunyExprNode] }
+    | { op: 'ternary', args: [condition: PunyExprNode, whenTrue: PunyExprNode, whenFalse: PunyExprNode] }
+  )
+
+  type PunyExprFunction = {
+    (context: { [name in string]: any }): any
+    toJSON(): PunyExprNode
+    toString(): string
+  }
+
+  type PunyExpr = {
+    (expression: string, options?: {
+      regex: true | ((pattern: string, flags: string) => RegExp)
+    }): PunyExprFunction
+    Error: typeof PunyExprError
+    version: string
+    tokenize(expression: string): PunyExprToken[]
+  }
+
+  const punyexpr: PunyExpr
 }
-
-type PunyExprToken = 
-  | ['literal', string | number | boolean, offset: number, length: number]
-  | ['identifier', string, offset: number, length: number]
-  | ['regex', [pattern: string, flags: string], offset: number, length: number]
-  | ['symbol', '+' | '-' | '*' | '/' | '[' | ']' | '.' | '?' | ':' | '%' | '<' | '=' | '>' | '!' | '&' | '|' | '(' | ')' | ',', offset: number, length: 1]
-  ;
-
-class PunyExprError extends Error {
-  offset: number;
-}
-
-export {};
